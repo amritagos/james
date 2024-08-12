dc8973ffc36319f45b16a36e5279750be4dad478
Amrita Goswami <amrita16thaug646@gmail.com>
2024-08-12 13:35:19 +0000

    system: Minor formatting change

--- james/include/system.hpp
+++ james/include/system.hpp
@@ -19,7 +19,7 @@ struct Atom {
   int type{}; // Type number of the atom (could be an atomic number, or type
               // number as in LAMMPS)
   std::optional<int> mol_id = std::nullopt; // Molecule identifier
-  std::vector<double> position{};     // Position vector 
+  std::vector<double> position{};           // Position vector
 };
 
 class System {
